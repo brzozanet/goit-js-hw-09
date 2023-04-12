19d4e2ac7bd809522b229c98ff51a9905081c038
brzozanet <pawel@brzoza.net>
2023-04-12 13:22:18 +0000

    Deploying to gh-pages from @ brzozanet/goit-js-hw-09@6dc19fd4fdde407ce52343b36b4bab210575b7d3 🚀

---
+++ 01-color-switcher.b2de49cf.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(a=>{o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(t=>{clearInterval(o),e.disabled=!1,d.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.b2de49cf.js.map
---
+++ 01-color-switcher.edfe18de.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let a=null;t.addEventListener("click",(l=>{a=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(e=>{clearInterval(a),t.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.edfe18de.js.map
