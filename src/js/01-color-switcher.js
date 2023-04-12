6dc19fd4fdde407ce52343b36b4bab210575b7d3
Paweł Brzozowski <pawel@brzoza.net>
2023-04-12 15:19:28 +0200

    Task #01 (color switcher)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyEl = document.querySelector("body");
+const startBtnEl = document.querySelector("button[data-start]");
+const stopBtnEl = document.querySelector("button[data-stop]");
 
+let colorRefresh = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startChangeColor = event => {
+  colorRefresh = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+};
+
+const stopChangeColor = event => {
+  clearInterval(colorRefresh);
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+};
+
+startBtnEl.addEventListener("click", startChangeColor);
+stopBtnEl.addEventListener("click", stopChangeColor);
